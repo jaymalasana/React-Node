91ecb2669ab006d2dfd774393c1d6d9a4705ff91
JayMalasana <jaymalasana051@gmail.com>
2024-06-11 14:36:54 +0530

    Validation Added

--- Frontend/front-end/src/components/AddProduct.js
+++ Frontend/front-end/src/components/AddProduct.js
@@ -5,11 +5,17 @@ const AddProduct = () => {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const [error, setError] = useState(false);
 
   const addProduct = async () => {
-    console.log(name, price, category, company);
+    // console.log(name, price, category, company);
+    if (!name || price || category || company) {
+      setError(true);
+      return false;
+    }
+
     const userId = JSON.parse(localStorage.getItem("user"))._id;
-    console.log(userId);
+    // console.log(userId);
 
     let result = await fetch("http://localhost:8080/add-product", {
       method: "post",
@@ -19,11 +25,11 @@ const AddProduct = () => {
       },
     });
     result = await result.json();
-    console.log(result);
+    // console.log(result);
   };
   return (
     <div className="bg-[#456990]">
-      <h1>Add Product Page</h1>
+      <h1 className="text-3xl text-center text-[#d6b880]">Login</h1>
       <input
         type="text"
         className="bg-[#516cbc] border border-gray-300 text-[#d6b880] text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#d6b880] mx-auto my-5"
@@ -31,6 +37,11 @@ const AddProduct = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      {error && !name && (
+        <span className="block text-red-700 font-medium text-center text-xl">
+          Enter valid name
+        </span>
+      )}
       <input
         type="text"
         className="bg-[#516cbc] border border-gray-300 text-[#d6b880] text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#d6b880] mx-auto my-5"
@@ -38,6 +49,11 @@ const AddProduct = () => {
         value={price}
         onChange={(e) => setPrice(e.target.value)}
       />
+      {error && !price && (
+        <span className="block text-red-700 font-medium text-center text-xl">
+          Enter valid name
+        </span>
+      )}
       <input
         type="text"
         className="bg-[#516cbc] border border-gray-300 text-[#d6b880] text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#d6b880] mx-auto my-5"
@@ -45,6 +61,11 @@ const AddProduct = () => {
         value={category}
         onChange={(e) => setCategory(e.target.value)}
       />
+      {error && !category && (
+        <span className="block text-red-700 font-medium text-center text-xl">
+          Enter valid name
+        </span>
+      )}
       <input
         type="text"
         className="bg-[#516cbc] border border-gray-300 text-[#d6b880] text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#d6b880] mx-auto my-5"
@@ -52,6 +73,11 @@ const AddProduct = () => {
         value={company}
         onChange={(e) => setCompany(e.target.value)}
       />
+      {error && !company && (
+        <span className="block text-red-700 font-medium text-center text-xl">
+          Enter valid name
+        </span>
+      )}
       <button
         type="button"
         className="mx-auto my-5 bg-[#516cbc] border border-gray-300 text-[#d6b880] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:bg-[#132048]"
