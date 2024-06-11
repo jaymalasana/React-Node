074893bda55745b576c101e4ea71b8fb4162b243
JayMalasana <jaymalasana051@gmail.com>
2024-06-11 18:41:01 +0530

    Prefilled Input For Update

--- Frontend/front-end/src/components/UpdateProduct.js
+++ Frontend/front-end/src/components/UpdateProduct.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+
+  const params = useParams();
+
+  useEffect(() => {
+    // console.log(params);
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    console.log(params);
+    let result = await fetch(`http://localhost:8080/product/${params.id}`);
+    result = await result.json();
+    setName(result.name)
+    setPrice(result.price)
+    setCategory(result.category)
+    setCompany(result.company);
+  };
 
   const updateProduct = async () => {
     console.log(name, price, category, company);
