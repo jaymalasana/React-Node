2579a57395ce0642d8e7b10e7600b96c33aa8a45
JayMalasana <jaymalasana051@gmail.com>
2024-06-12 10:54:10 +0530

    Search Api done

--- Backend/index.js
+++ Backend/index.js
@@ -52,24 +52,37 @@ app.delete("/product/:id", async (req, res) => {
 });
 
 app.get("/product/:id", async (req, res) => {
-  let result =  await Product.findOne({_id: req.params.id})
-  if(result) {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
     res.send(result);
-  }else{
-    res.send({result: "No record found."});
+  } else {
+    res.send({ result: "No record found." });
   }
-})
+});
 
-app.put('/product/:id', async (req, res) => {
-    let result = await Product.updateOne({
-      _id: req.params.id
+app.put("/product/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    {
+      _id: req.params.id,
     },
     {
-      $set: req.body
-    } 
-  )
+      $set: req.body,
+    }
+  );
   res.send(result);
-})
+});
+
+app.get("/search/:key", async (req, res) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { price: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } }
+    ],
+  });
+  res.send(result);
+});
 
 console.log("Server Running on port 8080");
 
